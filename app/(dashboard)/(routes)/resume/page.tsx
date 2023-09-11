2ba8231a99561fb8d07fd486f6fb3eedc6abce55
jackson1129 <jacksoncoco29@gmail.com>
2023-09-11 21:13:08 +0530

    music and video api's updated

--- app/(dashboard)/(routes)/resume/page.tsx
+++ app/(dashboard)/(routes)/resume/page.tsx
@@ -141,7 +141,7 @@ const ConversationPage = () => {
           </div>
         )}
         {messages.length === 0 && !isLoading && (
-          <Empty label="No conversation started." />
+          <Empty label="No resume generated." />
         )}
         <div className="flex flex-col-reverse gap-y-4">
           {messages.map((message) => (
